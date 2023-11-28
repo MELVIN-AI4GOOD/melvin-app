3647f6271c1b5026e13b3092d9371adaa26048c2
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-11-28 16:49:38 -0300

    ✨ add basic question frame

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,14 +1,61 @@
 import React from "react";
 
-class Gym extends React.Component {
+class Question extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedOption: null,
+            answered: false,
+        };
+    }
+
+    handleOptionChange = (option) => {
+        this.setState({
+            selectedOption: option,
+        });
+    };
+
+    handleAnswer = () => {
+        if (this.state.selectedOption !== null) {
+            // Aqui você pode adicionar lógica para verificar se a resposta está correta
+            // e atualizar o estado conforme necessário.
+            // Por enquanto, apenas marcamos a pergunta como respondida.
+            this.setState({
+                answered: true,
+            });
+        } else {
+            // Caso o usuário tente responder sem selecionar uma alternativa.
+            alert('Nenhuma alternativa selecionada!');
+        }
+    };
+
     render() {
+        const { statement, options } = this.props;
+        const { selectedOption, answered } = this.state;
+
         return (
             <div>
-                <h1>Gym</h1>
-                <hr></hr>
+                <p>{statement}</p>
+                {options.map((option, index) => (
+                    <div key={index}>
+                        <label>
+                            <input
+                                type="radio"
+                                value={option}
+                                checked={selectedOption === option}
+                                onChange={() => this.handleOptionChange(option)}
+                                disabled={answered}
+                            />
+                            {`Alternativa ${String.fromCharCode(65 + index)}: ${option}`}
+                        </label>
+                    </div>
+                ))}
+                <button onClick={this.handleAnswer} disabled={answered}>
+                    Responder
+                </button>
             </div>
         );
     }
 }
 
-export default Gym;
+export default Question;
